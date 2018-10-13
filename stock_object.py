fdd2286083661359e159fc25969977f2787984ae
Kane Knight <kk2216@ic.ac.uk>
2018-10-13 23:43:45 +0100

    write init for stock object

--- stock_object.py
+++ stock_object.py
@@ -1,2 +1,24 @@
 class Stock:
-    def __init__(self):
+    def __init__(self, currentPrice, fiftyDayMvAvg, twoHundredDayMvAvg,
+                 marketCap, trailingPe, forwardPe, peg, priceSales, priceBook,
+                 evSales, evEBITDA, debtEquity, currentRatio, operatingCashFlow,
+                 leveredFreeCashFlow, forwardDividendYield, trailingDividendYield,
+                 PayoutRatio):
+        self.currentPrice          = currentPrice
+        self.fiftyDayMvAvg         = fiftyDayMvAvg
+        self.twoHundredDayMvAvg    = twoHundredDayMvAvg
+        self.marketCap             = marketCap
+        self.trailingPe            = trailingPe
+        self.forwardPe             = forwardPe
+        self.peg                   = peg
+        self.priceSales            = priceSales
+        self.priceBook             = priceBook
+        self.evSales               = evSales
+        self.evEBITDA              = evEBITDA
+        self.debtEquity            = debtEquity
+        self.currentRatio          = currentRatio
+        self.operatingCashFlow     = operatingCashFlow
+        self.leveredFreeCashFlow   = leveredFreeCashFlow
+        self.forwardDividendYield  = forwardDividendYield
+        self.trailingDividendYield = trailingDividendYield
+        self.PayoutRatio           = PayoutRatio
