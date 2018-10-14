24e441ecb5400359cd6040fffdab3f795d067521
Kane Knight <kk2216@ic.ac.uk>
2018-10-14 03:56:47 +0100

    Print dataframe ordered by debt to equity.

--- stock_object.py
+++ stock_object.py
@@ -1,24 +1,10 @@
 class Stock:
-    def __init__(self, currentPrice, fiftyDayMvAvg, twoHundredDayMvAvg,
-                 marketCap, trailingPe, forwardPe, peg, priceSales, priceBook,
-                 evSales, evEBITDA, debtEquity, currentRatio, operatingCashFlow,
-                 leveredFreeCashFlow, forwardDividendYield, trailingDividendYield,
-                 PayoutRatio):
-        self.currentPrice          = currentPrice
-        self.fiftyDayMvAvg         = fiftyDayMvAvg
-        self.twoHundredDayMvAvg    = twoHundredDayMvAvg
-        self.marketCap             = marketCap
-        self.trailingPe            = trailingPe
-        self.forwardPe             = forwardPe
-        self.peg                   = peg
-        self.priceSales            = priceSales
-        self.priceBook             = priceBook
-        self.evSales               = evSales
-        self.evEBITDA              = evEBITDA
-        self.debtEquity            = debtEquity
-        self.currentRatio          = currentRatio
-        self.operatingCashFlow     = operatingCashFlow
-        self.leveredFreeCashFlow   = leveredFreeCashFlow
-        self.forwardDividendYield  = forwardDividendYield
-        self.trailingDividendYield = trailingDividendYield
-        self.PayoutRatio           = PayoutRatio
+    __allowed = ["ticker", "currentPrice", "fiftyDayMvAvg", "twoHundredDayMvAvg",
+                 "marketCap", "trailingPe", "forwardPe", "peg", "priceSales", "priceBook",
+                 "evSales", "evEBITDA", "debtEquity", "currentRatio", "operatingCashFlow",
+                 "leveredFreeCashFlow", "forwardDividendYield", "trailingDividendYield",
+                 "PayoutRatio"]
+    def __init__(self, **kwargs):
+        for k, v in kwargs.items():
+            assert (k in self.__class__.__allowed)
+            setattr(self, k, v)
--- stock_screener.py
+++ stock_screener.py
@@ -4,14 +4,31 @@ import html_parser as parser
 import os
 import pandas as pd
 import requests
+from stock_object import Stock
 
 #Where tickers comes from
 goldStocks = "http://www.miningfeeds.com/gold-mining-report-all-countries"
 
 baseurl = "https://uk.finance.yahoo.com/quote/"
 
-unwantedRows = []
-
+wantedRows = {60 : "currentPrice",
+              36 : "fiftyDayMvAvg",
+              37 : "twoHundredDayMvAvg",
+              0  : "marketCap",
+              2  : "trailingPe",
+              3  : "forwardPe",
+              4  : "peg",
+              5  : "priceSales",
+              6  : "priceBook",
+              7  : "evSales",
+              8  : "evEBITDA",
+              26 : "debtEquity",
+              27 : "currentRatio",
+              29 : "operatingCashFlow",
+              30 : "leveredFreeCashFlow",
+              50 : "forwardDividendYield",
+              52 : "trailingDividendYield",
+              54 : "PayoutRatio"}
 
 class StockScreener:
     def __init__(self, stockTypes, baseurl = baseurl, goldStocks = goldStocks, reloadTickers = True):
@@ -30,12 +47,15 @@ class StockScreener:
         for ticker in self.tickers:
             dataFile = "./%s/%s.csv" % (dataDir, ticker)
             data = None
+            if ticker == "GLF.AX":
+                continue
             if not os.path.exists(dataFile):
                 data = self.saveDataAndReturnFrame(dataFile, ticker)
             else:
                 print("Already have %s" % ticker)
-                data = pd.read_csv(dataFile)
-            self.createStock(data)
+                data = pd.read_csv(dataFile, index_col=0)
+            self.stocks.append(self.createStock(data, ticker))
+        self.calculateAveragesForSector()
 
 
     def loadGoldTickers(self, stockTypes = "gold_stocks"):
@@ -63,19 +83,40 @@ class StockScreener:
         frames = pd.read_html(data.text)
         if len(frames) < 3:
             print("No data for: %s" % ticker)
+            return
 
         #Join stats and price
         frames.append(priceFrame)
         frame = pd.concat(frames, ignore_index=True)
-        print(frame)
 
         #Save data
         frame.to_csv(dataFile)
         return frame
 
-    def createStock(self, stockDataFrame):
-        for index, row in stockDataFrame.iterrows():
-            pass
+    def createStock(self, stockDataFrame, ticker):
+        dict = {}
+        for index in wantedRows:
+            series = stockDataFrame.loc[index]
+            key = wantedRows[index]
+            value = series.get(1)
+            dict[key] = value
+            dict["ticker"] = ticker
+        return Stock(**dict)
+
+    def calculateAveragesForSector(self):
+        seriesList = []
+        tickers = []
+        for stock in self.stocks:
+            dict = stock.__dict__
+            series = pd.Series(dict)
+            series = series.drop(labels=["ticker"])
+            tickers.append(stock.ticker)
+            seriesList.append(series)
+        frame = pd.concat(seriesList, axis=1, keys=tickers)
+        frame = frame.sort_values(by="debtEquity", axis=1, ascending=True)
+        print(frame)
+        frame.to_csv("output.csv")
+
 
 def main():
     stockScreener = StockScreener("gold_stocks", reloadTickers = False)
